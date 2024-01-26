945dc86d6be36986af9af59b03619aec2dea6a6d
Julie <julienijs15@gmail.com>
2024-01-26 14:19:47 +0100

    update code: elastic net regression visualization

--- Keep_Ving_Elastic_Net_Regression.R
+++ Keep_Ving_Elastic_Net_Regression.R
@@ -17,7 +17,7 @@ keep <- subset(keep, ID!="double")
 
 subset <- keep[, c("Construction", "author", "Animacy", "Verb_innovation")]
 
-# convert author & Change to factor
+# Convert author & Change to factor
 subset$Construction <- as.factor(subset$Construction)
 subset$author <- as.factor(subset$author)
 subset$Animacy <- as.factor(subset$Animacy)
@@ -28,15 +28,15 @@ ds <- dataset(df=subset,
               to_binary_columns=c("author"),
               other_columns=c("Animacy", "Verb_innovation"))
 
-# convert the data to a feature matrix
+# Convert the data to a feature matrix
 feature_matrix <- ds$as_matrix()
 
-# retrieve feature list
+# Retrieve feature list
 feature_list <- ds$as_feature_list()
 
 #### Perform elastic net regression ####
 
-# define net object
+# Define net object
 net <- elastic_net(ds=ds,
                    nfolds=20,
                    type.measure="class")
@@ -50,10 +50,10 @@ fit <- net$do_elastic_net_regression(alpha=0.6)
 
 coefficients_with_labels <- net$attach_coefficients(fit)
 
-# save to csv
+# Save to csv
 write.csv(coefficients_with_labels, "elastic_net_output.csv", row.names=FALSE)
 
-# visualisation
+#### Visualization with probabilities ####
 
 barplot(coefficients_with_labels$coefficient, 
         names.arg = coefficients_with_labels$feature, 
@@ -80,4 +80,52 @@ text(x = probabilities, y = seq_along(probabilities),
 axis(1, at = seq(0, 1, by = 0.1), 
      labels = c("complex-transitive", "0.1", "0.2", "0.3", "0.4", "0.5", "0.6", "0.7", "0.8", "0.9", "continuative"), 
      adj = 0.5)
+
+#### Visualization when the subject is inanimate ####
+
+# Find the coefficient of _is_Inanimate
+is_inanimate_coefficient <- coefficients_with_labels$coefficient[coefficients_with_labels$feature == "_is_Inanimate"]
+
+# Calculate probabilities when subject is inanimate
+coefficients_with_inanimate <- coefficients_with_labels$coefficient + is_inanimate_coefficient
+probabilities_with_inanimate <- logistic(coefficients_with_inanimate)
+
+# Create scatter plot with probabilities including the effect of _is_Inanimate
+plot(x = probabilities_with_inanimate, y = seq_along(probabilities_with_inanimate), pch = 16, col = "black",
+     xlab = "", ylab = "",
+     xlim = c(0, 1), xaxt = "n")
+
+# Add feature labels
+text(x = probabilities_with_inanimate, y = seq_along(probabilities_with_inanimate), 
+     labels = coefficients_with_labels$feature, pos = 4, cex = 0.8)
+
+# Customize x-axis labels with adjusted position
+axis(1, at = seq(0, 1, by = 0.1), 
+     labels = c("complex-transitive", "0.1", "0.2", "0.3", "0.4", "0.5", "0.6", "0.7", "0.8", "0.9", "continuative"), 
+     adj = 0.5)
+
+
+#### Visualization when the verb is innovative ####
+
+# Find the coefficient of _is_Innovative_verb
+is_innovative_coefficient <- coefficients_with_labels$coefficient[coefficients_with_labels$feature == "_is_Innovative verb"]
+
+# Calculate probabilities when verb is innovative
+coefficients_with_innovative <- coefficients_with_labels$coefficient + is_innovative_coefficient
+probabilities_with_innovative <- logistic(coefficients_with_innovative)
+
+# Create scatter plot with probabilities including the effect of _is_Innovative_verb
+plot(x = probabilities_with_innovative, y = seq_along(probabilities_with_innovative), pch = 16, col = "black",
+     xlab = "", ylab = "",
+     xlim = c(0, 1), xaxt = "n")
+
+# Add feature labels
+text(x = probabilities_with_innovative, y = seq_along(probabilities_with_innovative), 
+     labels = coefficients_with_labels$feature, pos = 4, cex = 0.8)
+
+# Customize x-axis labels with adjusted position
+axis(1, at = seq(0, 1, by = 0.1), 
+     labels = c("complex-transitive", "0.1", "0.2", "0.3", "0.4", "0.5", "0.6", "0.7", "0.8", "0.9", "continuative"), 
+     adj = 0.5)
+
 
